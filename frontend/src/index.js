8f120bf698680b6d3881eb3b53d5f823bdd4b4d7
Qinjie <mark.qj@gmail.com>
2021-08-24 08:20:04 +0800

    Replace frontend with reactjs typescript

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './auth';
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
